ba36cd16679529065fee8c181ad61a9b59915085
adintr <webmaster@adintr.com>
2017-06-18 11:44:40 +0800

    增加 analyzePack 函数, 方便把抓的包解析成对象进行分析

--- HelpFunc.py
+++ HelpFunc.py
@@ -1,4 +1,5 @@
 import re
+from SFSDecoder import decodeObject, SFSBuffer
 
 def printByteArray(data):
     shtr = str(data).encode('hex')
@@ -12,3 +13,9 @@ def readByteArray(str):
     str = str.replace('\r', '')
     str = str.replace('\t', '')
     return bytearray(str.decode('hex'))
+
+# UseAge: analyzePack("12 00 03 00 01 61 03 00 00 00 01 63 02 00 00 01 70 12 00 03 00 03 62 69 6E 01 01 00 02 63 6C 08 00 07 41 6E 64 72 6F 69 64 00 03 61 70 69 08 00 05 31 2E 36 2E 31")
+def analyzePack(strhex):
+    packdata = readByteArray(strhex)
+    printByteArray(packdata)
+    print decodeObject(SFSBuffer(packdata))
